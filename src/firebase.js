3a62bbab16755ad96ce9639fd7ad28ad851480ba
Aditya kumar gope <89683134+mrGope@users.noreply.github.com>
2024-02-03 14:26:12 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfUDfgUSOY8il9C2AsAjzUKgW5UKOyNCI",
-  authDomain: "blog-mate.firebaseapp.com",
-  databaseURL: "https://blog-mate-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "blog-mate",
-  storageBucket: "blog-mate.appspot.com",
-  messagingSenderId: "531321647592",
-  appId: "1:531321647592:web:d42acdc3c4bbeb8d3f345d"
+//enter your config file.
 };
 
 const app = initializeApp(firebaseConfig);
